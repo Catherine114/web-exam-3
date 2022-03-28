b1816f911365c4350ef74f60e97305c8523f1bf3
Catherine114 <62448379+Catherine114@users.noreply.github.com>
2022-03-28 14:55:17 +0300

    main page

---
+++ components/WidgetComponent.vue
@@ -0,0 +1,56 @@
+<template>
+  <div v-if="info" class="widget__wrapper">
+    <div class="widget">
+      <span>{{ info.cows }}</span> <br />коров
+    </div>
+    <div class="widget">
+      <span>{{ info.rabbits }}</span> <br />
+      кроликов
+    </div>
+    <div class="widget">
+      <span>{{ info.sheeps }}</span> <br />
+      овечек
+    </div>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'WidgetComponent',
+  data() {
+    return {
+      info: null,
+    }
+  },
+  computed: {
+    ...mapGetters({
+      infoData: 'mainpage/getWidget',
+    }),
+  },
+  async mounted() {
+    const data = await this.infoData
+    this.info = data.data
+  },
+}
+</script>
+
+<style>
+.widget {
+  padding: 20px;
+  border-radius: 10px;
+  background: #cce6ea94;
+  -webkit-box-shadow: 4px 4px 0px 0px rgb(34 60 80 / 20%);
+  -moz-box-shadow: 4px 4px 0px 0px rgb(34 60 80 / 20%);
+  box-shadow: 4px 4px 0px 0px rgb(34 60 80 / 20%);
+}
+.widget__wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: stretch;
+  margin: 20px 20px;
+}
+.widget span {
+  font-size: 42px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,66 @@
 <template>
-  <Tutorial />
+  <div class="page__container">
+    <WidgetComponent />
+    <div v-if="info">
+      <div class="content">
+        <h1 class="heading">Главная страница</h1>
+
+        <img class="mainimage" :src="info.image" alt="" />
+      </div>
+      <p v-html="info.text" class="main__text"></p>
+    </div>
+    <LoaderComponent v-else />
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import WidgetComponent from '@/components/WidgetComponent.vue'
+import LoaderComponent from '@/components/LoaderComponent.vue'
 export default {
   name: 'IndexPage',
+  components: { LoaderComponent, WidgetComponent },
+  layout: 'DefaultLayout',
+  data() {
+    return {
+      info: null,
+    }
+  },
+  computed: {
+    ...mapGetters({
+      infoData: 'mainpage/getMainPage',
+    }),
+  },
+  async mounted() {
+    const data = await this.infoData
+    this.info = data.data
+  },
 }
 </script>
+
+<style>
+.mainimage {
+  display: block;
+  border-radius: 30px;
+  max-width: 600px;
+}
+.main__text {
+  padding: 20px;
+}
+.delivery img {
+  border-radius: 20px;
+}
+.content {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+}
+.heading {
+  text-align: center;
+}
+.page__container {
+  display: flex;
+  margin-bottom: 40px;
+}
+</style>
